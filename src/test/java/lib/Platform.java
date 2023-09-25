b959a49fa519afdacc88725eca6c62ff51e8649e
Sergey <naskar0071@mail.ru>
2023-09-26 00:16:20 +0500

    tplTests

--- src/test/java/lib/Platform.java
+++ src/test/java/lib/Platform.java
@@ -6,50 +6,26 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.URL;
 
 public class Platform {
-    private static final String PLATFORM_ANDROID = "android";
-    private static final String APPIUM_ANDROID_URL = "http://0.0.0.0:4723";
-    private static Platform instance;
-    public Platform(RemoteWebDriver driver){}
-
-    public static Platform getInstance() {
-
-        return instance;
-    }
+    protected RemoteWebDriver driver;
 
     public static RemoteWebDriver getDriver() throws Exception {
-        URL URL_ANDROID = new URL(APPIUM_ANDROID_URL);
-        if (this.isAndroid()){
-            return new AndroidDriver(URL_ANDROID, this.getAndroidDesiredCapabilities());
-        }else throw new Exception("Cannot detect type of the driver. Platform value: " + this.getPlatformVar());
+        URL URL = new URL("http://0.0.0.0:4723");
+        return new AndroidDriver(URL, getAndroidDesiredCapabilities());
     }
 
-    public boolean isAndroid() {return isPlatform();}
-
-    private DesiredCapabilities getAndroidDesiredCapabilities(){
+    private static DesiredCapabilities getAndroidDesiredCapabilities(){
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("autoGrantPermissions", true);
         capabilities.setCapability("unicodeKeyboard", "true");
         capabilities.setCapability("resetKeyboard", "true");
 
         capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "and11");
-        capabilities.setCapability("platformVersion", "11.0");
+        capabilities.setCapability("deviceName", "and13");
+        capabilities.setCapability("platformVersion", "13.0");
         capabilities.setCapability("automationName", "UiAutomator2");
         capabilities.setCapability("appPackage", "com.riesapp.debug");
         capabilities.setCapability("appActivity", "com.riesapp.features.root.RootActivity");
         capabilities.setCapability("app", "C:\\Users\\SERGEY\\Documents\\UITests\\apk\\ries-ries-debug.apk");
         return capabilities;
     }
-
-
-    private boolean isPlatform() {
-        String platform = this.getPlatformVar();
-        return Platform.PLATFORM_ANDROID.equals(platform);
-    }
-
-    public String getPlatformVar()
-    {
-        return System.getenv("PLATFORM");
-    }
-
 }
